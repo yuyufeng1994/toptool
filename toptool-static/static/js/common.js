357d9e60bfc59078c33e62e9407e1a015b3897a7
yuyufeng <416530244@qq.com>
2018-06-28 16:50:59 +0800

    commit

--- toptool-static/static/js/common.js
+++ toptool-static/static/js/common.js
@@ -9,7 +9,20 @@ $(function () {
             $(this).addClass("active")
         }
     })
-})
+
+    //百度推送
+        var bp = document.createElement('script');
+        var curProtocol = window.location.protocol.split(':')[0];
+        if (curProtocol === 'https') {
+            bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+        }
+        else {
+            bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+        }
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(bp, s);
+
+});
 
 var loadContent = function (pageUrl) {
     $.get("page/" + pageUrl, function (res) {
